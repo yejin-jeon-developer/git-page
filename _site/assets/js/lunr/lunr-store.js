42040c368b15750254064a3aaa334171d3de3c3b
jeon_yejin <yemong1226@gmaill.com>
2022-03-29 17:12:45 +0900

    수정

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Test",
+        "excerpt":"Welcome   Hello world~~~                                                                                                                                This is a second gallery example with images hosted externally.      ","categories": [],
+        "tags": [],
+        "url": "/portfolio/2022-03-29-test/",
+        "teaser": "https://farm2.staticflickr.com/1272/4697500467_8294dac099_q.jpg"
+      },{
+        "title": "Test2",
+        "excerpt":"Welcome   test2  ","categories": ["react"],
+        "tags": [],
+        "url": "/react/test2/",
+        "teaser": null
+      },{
+        "title": "Spring boot",
+        "excerpt":"Welcome   Hello world                                                                                                                                This is a second gallery example with images hosted externally.      ","categories": ["spring-boot"],
+        "tags": [],
+        "url": "/spring-boot/test/",
+        "teaser": null
+      }]
